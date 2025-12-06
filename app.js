58d8c493b22964774e9a3497dbddf1ba236fa43c
kkiyakk <166090798+kkiyakk@users.noreply.github.com>
2025-12-07 02:10:49 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 require('dotenv').config();
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const passport = require('passport');
-
+const  passport = require('passport');
+// require('./app_server/models/db');
 require('./app_api/models/db');
 require('./app_api/config/passport');
 
-// ❌ 기존 express-generator 라우터는 안 씀
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-
-// ✅ Loc8r용 라우터만 사용
-const indexRouter = require('./app_server/routes/index');
+// const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
 const apiRouter = require('./app_api/routes/index');
-// const aboutRouter = require('./app_server/routes/about');
-// const usersRouter = require('./app_server/routes/users');
-
-const app = express();
+var app = express();
 
 const cors = require('cors');
 const corsOptions = {
@@ -31,10 +23,7 @@ app.use(cors(corsOptions));
 
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-with, Content-type, Accept, Authorization'
-  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-type, Accept, Authorization');
   next();
 });
 
@@ -47,49 +36,47 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_public', 'build', 'browser')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 app.use(passport.initialize());
 
-// CORS 설정
+
 app.use('/api', (req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-type, Accept, Authorization");
   next();
 });
 
-// 서버 사이드 페이지
-app.use('/', indexRouter);
 
-// users 라우트는 안 씀
-// app.use('/users', usersRouter);
-
-// API
+// app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
+// app.get('*', function(req, res, next) {
+//   res.sendFile(path.join(__dirname, 'app_public', 'build', index.html));
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
     res
       .status(401)
       .json({"message" : err.name + ": " + err.message});
   }
-});
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+})
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
