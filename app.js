5431f198cf5a2cac344bf061c79a2f5b0441545d
kkiyakk <166090798+kkiyakk@users.noreply.github.com>
2025-12-09 20:36:02 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -4,28 +4,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const  passport = require('passport');
 // require('./app_server/models/db');
 require('./app_api/models/db');
-require('./app_api/config/passport');
 
-// const indexRouter = require('./app_server/routes/index');
+const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const apiRouter = require('./app_api/routes/index');
+
 var app = express();
-
-const cors = require('cors');
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // For legacy browser support
-};
-app.use(cors(corsOptions));
-
-app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-type, Accept, Authorization');
-  next();
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
@@ -36,9 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_public', 'build')));
-app.use(passport.initialize());
-
 
 app.use('/api', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -46,26 +29,14 @@ app.use('/api', (req, res, next) => {
   next();
 });
 
-
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-
-// app.get('*', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, 'app_public', 'build', index.html));
-// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-app.use((err, req, res, next) => {
-  if (err.name === 'UnauthorizedError') {
-    res
-      .status(401)
-      .json({"message" : err.name + ": " + err.message});
-  }
-})
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -77,6 +48,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
